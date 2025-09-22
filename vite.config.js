346501b7f7048cd252ebc68ad15f8934cb56576b
Blacksider1 <adamtsurov123@gmail.com>
2025-09-22 11:48:11 +0300

    finish project

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/yeausers',
+    base: '/yeausers/',
     resolve: {
         alias: {
             '@assets': path.resolve(__dirname, 'src/assets'),

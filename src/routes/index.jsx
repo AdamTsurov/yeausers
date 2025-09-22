27e9b5d6b0535ee8c5b8f907a10dbaaa651d494a
Blacksider1 <adamtsurov123@gmail.com>
2025-09-22 13:37:48 +0300

    finish project

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,8 @@
 import FavoriteUsersPage from '@pages/FavoriteUsersPage/FavoriteUsersPage'
 import UsersPage from '@pages/UsersPage/UsersPage'
-import { Navigate } from 'react-router'
 
 export const routes = [
     { path: '/', component: <UsersPage />, index: true },
     { path: '/favorite-users', component: <FavoriteUsersPage /> },
-    { path: '*', component: <Navigate to="/" replace={true} /> },
+    { path: '*', component: <div>Пусто</div> },
 ]

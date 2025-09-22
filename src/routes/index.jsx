16b749f2c7b7b45b3f1c3b567700e7c009db6d48
Blacksider1 <adamtsurov123@gmail.com>
2025-09-22 11:45:52 +0300

    finish project

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,7 @@ import FavoriteUsersPage from '@pages/FavoriteUsersPage/FavoriteUsersPage'
 import UsersPage from '@pages/UsersPage/UsersPage'
 
 export const routes = [
-    { path: '/yeausers', component: <UsersPage />, index: true },
+    { path: '/', component: <UsersPage />, index: true },
     { path: '/favorite-users', component: <FavoriteUsersPage /> },
     { path: '*', component: <div>Пусто</div> },
 ]
